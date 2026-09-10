34e26b6a45505dc21f1513924929434c2543947a
Aryan raj <aryanbanthu@gmail.com>
2026-09-10 15:51:27 +0530

    feat: add Spring Data JPA repositories for User, Category, and Expense

---
+++ src/main/java/com/expensetracker/expense_tracker_api/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.expensetracker.expense_tracker_api.repository;
+
+import com.expensetracker.expense_tracker_api.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
